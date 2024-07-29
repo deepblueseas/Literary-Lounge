d88f0b17ed478e2c5c88507bcd436fd0e7550e13
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-29 19:59:03 -0400

    fixed app.jsx for now hopefully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
-import Header from './components/Header';
-import Rating from './components/Rating';
-import Footer from './components/Footer';
+
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
