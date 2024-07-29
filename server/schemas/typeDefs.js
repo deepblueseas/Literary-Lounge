92a08555eef4448ad5648a2857da50e1be61a8a0
rvanetta97 <rachel.vanetta@gmail.com>
2024-07-29 19:22:47 -0400

    mutations/queries changes/additions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,13 @@
 const typeDefs = `
+    scalar Date
+
  type Book {
     authors: String!
     description: String!
     bookId: String!
     genre: String
-    publishedDate: Date
     summary: String
+    publishedDate: Date
   }
 
   type User {
@@ -21,13 +23,41 @@ type Bookclub {
     members: [User]
     savedBooks: [Book]
 }
+    input BookInput {
+    authors: String!
+    description: String!
+    bookId: String!
+    genre: String
+    summary: String
+    publishedDate: Date
+  }
+
+  input BookclubInput {
+    name: String!
+    description: String!
+    members: [String]
+    savedBooks: [BookInput]
+  }
+
+type Auth {
+    token: ID!
+    user: User
+  }
 
 type Query {
-
+    users: [User]
+    user(username: String!): User
+    books: [Book]
+    bookClubs: [Bookclub]
   }
 
   type Mutation {
-
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: ID!): User
+    saveBookclub(book: BookclubInput!): User
+    removeBookclub(BookclubId: ID!): User
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+require ('dotenv').config()
+const  express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
@@ -28,7 +29,7 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('*', (_, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
