cfd06f7a49d32dfe2d80efe8c42e8d6045f6163c
duncanfd24 <159070352+duncanfd24@users.noreply.github.com>
2024-07-29 20:12:07 -0400

    Added footer, header, updatebook, and updatebookclub components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
