9880879f0cec92a92defa98a4a8da47fd0a7090c
dom <dominic.decapite@gmail.com>
2024-07-31 21:21:15 -0400

    Refactor Book list

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_BOOKS } from '../queries';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_BOOKS } from "../queries";
+import {
+  Box,
+  Heading,
+  Text,
+  UnorderedList,
+  ListItem,
+  Spinner,
+  Alert,
+  AlertIcon,
+} from "@chakra-ui/react";
 
 const BookList = () => {
   const { loading, error, data } = useQuery(GET_BOOKS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return <Spinner size="xl" />;
+  if (error)
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        Error fetching books
+      </Alert>
+    );
 
   return (
-    <div>
-      <h2>Books</h2>
-      <ul>
+    <Box maxWidth="600px" mx="auto" mt={5} p={5} borderWidth="1px" borderRadius="lg">
+      <Heading as="h2" size="lg" mb={5}>Books</Heading>
+      <UnorderedList>
         {data.books.map(({ id, title, author }) => (
-          <li key={id}>{title} by {author}</li>
+          <ListItem key={id} mb={2}>
+            <Text  fontWeight="bold">{title}</Text>
+            <Text>by {author}</Text>
+             
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </UnorderedList>
+    </Box>
   );
 };
 
