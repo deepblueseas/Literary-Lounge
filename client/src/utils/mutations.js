128292983da807996cf594edddea5e9f83fe16f2
duncanfd24 <159070352+duncanfd24@users.noreply.github.com>
2024-07-31 21:46:38 -0400

    Updated queries and resolvers.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,168 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+// Mutation to add a new user
+export const MUTATION_ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+// Mutation to add a new book
+export const MUTATION_ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $genre: String!, $publishedDate: String!, $summary: String!) {
+    addBook(title: $title, author: $author, genre: $genre, publishedDate: $publishedDate, summary: $summary) {
+      _id
+      title
+      author
+      genre
+      publishedDate
+      summary
+    }
+  }
+`;
+
+// Mutation to update a user
+export const MUTATION_UPDATE_USER = gql`
+  mutation updateUser($id: ID!, $email: String!) {
+    updateUser(id: $id, email: $email) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+// Mutation to update a book
+export const MUTATION_UPDATE_BOOK = gql`
+  mutation updateBook($id: ID!, $summary: String!) {
+    updateBook(id: $id, summary: $summary) {
+      _id
+      title
+      author
+      summary
+    }
+  }
+`;
+
+// Mutation to delete a user
+export const MUTATION_DELETE_USER = gql`
+  mutation deleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      _id
+      username
+    }
+  }
+`;
+
+// Mutation to delete a book
+export const MUTATION_DELETE_BOOK = gql`
+  mutation deleteBook($id: ID!) {
+    deleteBook(id: $id) {
+      _id
+      title
+    }
+  }
+`;
+
+// Mutation to add a book to a user's saved books
+export const MUTATION_ADD_BOOK_TO_USER = gql`
+  mutation addBookToUser($userId: ID!, $bookId: ID!) {
+    addBookToUser(userId: $userId, bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        _id
+        title
+      }
+    }
+  }
+`;
+
+// Mutation to remove a book from a user's saved books
+export const MUTATION_REMOVE_BOOK_FROM_USER = gql`
+  mutation removeBookFromUser($userId: ID!, $bookId: ID!) {
+    removeBookFromUser(userId: $userId, bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        _id
+        title
+      }
+    }
+  }
+`;
+
+// Mutation to add a user to a book club
+export const MUTATION_ADD_USER_TO_BOOKCLUB = gql`
+  mutation addUserToBookClub($userId: ID!, $bookclubId: ID!) {
+    addUserToBookClub(userId: $userId, bookclubId: $bookclubId) {
+      _id
+      name
+      members {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Mutation to remove a user from a book club
+export const MUTATION_REMOVE_USER_FROM_BOOKCLUB = gql`
+  mutation removeUserFromBookClub($userId: ID!, $bookclubId: ID!) {
+    removeUserFromBookClub(userId: $userId, bookclubId: $bookclubId) {
+      _id
+      name
+      members {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Mutation to update book list
+export const MUTATION_UPDATE_BOOK_LIST = gql`
+  mutation updateBookList($id: ID!, $name: String!) {
+    updateBookList(id: $id, name: $name) {
+      _id
+      name
+    }
+  }
+`;
+
+// Mutation to add book to a book list
+export const MUTATION_ADD_BOOK_TO_LIST = gql`
+  mutation addBookToList($listId: ID!, $bookId: ID!) {
+    addBookToList(listId: $listId, bookId: $bookId) {
+      _id
+      name
+      books {
+        _id
+        title
+      }
+    }
+  }
+`;
+
+// Mutation to remove book from a book list
+export const MUTATION_REMOVE_BOOK_FROM_LIST = gql`
+  mutation removeBookFromList($listId: ID!, $bookId: ID!) {
+    removeBookFromList(listId: $listId, bookId: $bookId) {
+      _id
+      name
+      books {
+        _id
+        title
+      }
+    }
+  }
+`;
---
+++ server/seeders/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS literary_lounge_db;
+
+CREATE DATABASE literary_lounge_db;
+
