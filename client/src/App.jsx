bc8ef11b711018d2df6e413e9dbb7998e41d56ba
Tess McGovern <ren.mcgovern@gmail.com>
2024-08-01 19:51:46 -0400

    got the backend routes all set with the right methods for our beloved bestie miss SQL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Rating from './components/Rating';
 
 
 import { ChakraProvider } from '@chakra-ui/react'
@@ -48,7 +47,6 @@ function App() {
           <Header />
           <div className="container">
             <Outlet />
-            <Rating />
           </div>
           <Footer />
         </div>
